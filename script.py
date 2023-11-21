4a5031c266f49874c4ff6b7cf22ca86ebdc71265
swann <swann-emilien.maillefert@ensae.fr>
2023-11-21 09:43:57 +0000

    Tests statistiques et suite modèle ECM
    Ajout des différents tests statistiques d'autocorrélation, de co-intégration, de 'blancheur' des résidus, d'hétéroscédasticité, test de rupture de Chow
    Procédure de Cochrane-Orcutt, modélisation ECM en 2 étapes (relation de long terme et de court terme) et procédure de Stock Watson (pour les ECM)

--- script.py
+++ script.py
@@ -63,8 +63,7 @@ print('p-value',IPC_tot_adf[1])
 
 
 
-##Modele Error Correction Model
-##On régresse d'abord les coûts de l'énergie sur les prix de production globaux
+##On régresse d'abord les coûts de l'énergie sur les prix de production globaux en stationnarisant les séries
 data = pd.merge(energie_prix_import,IPP_tot, on='TIME_PERIOD', how="inner")
 ##On différencie les données pour travailler avec des séries stationnaires
 data_diff = data.diff().dropna()
@@ -100,14 +99,120 @@ model = sm.OLS(y, X)
 results = model.fit()
 print(results.summary())
 
-##Autre méthode avec les modèles VAR
+##Test de la relation de co-intégration
 from statsmodels.tsa.api import VAR, coint
 data = pd.merge(energie_prix_import,IPP_tot, on='TIME_PERIOD', how="inner")
 cointegration_test = coint(data['OBS_VALUE_x'],data['OBS_VALUE_y'])
 print('p-value pour test de cointegration',cointegration_test[1])
-##p-valeur = 0.41
-#modelisation (a creuser)
-model = VAR(data)
-lag_order=3
-model_fitted = model.fit(lag_order)
-print(model_fitted.summary())
+##p-valeur = 0.41 Donc il existe bien une relation de co-intégration entre nos variables
+
+
+##Raisonnement économétrique menant aux modèles ECM
+data = pd.merge(energie_prix_import,IPP_tot, on='TIME_PERIOD', how="inner")
+##Simple régression OLS
+X = sm.add_constant(data[['OBS_VALUE_x']])
+y = data['OBS_VALUE_y']
+model = sm.OLS(y, X)
+results = model.fit()
+print(results.summary())
+##Comme la variable endogène n'est pas présente avec retard, on peut tester l'autocorrélation des résidus directement avec le test de Durbin-Watson
+##le summary nous donne une valeur de 0.049, or pour que les résidus ne soient pas auto-corrélés à l'ordre 1, DW doit être proche de 2 (en pratique
+##on accepte des modèle avec DW entre 1.5 et 2.5
+##pour afficher directement la stat DW : 
+from statsmodels.stats.stattools import durbin_watson
+DW_stat = durbin_watson(results.resid)
+print("La statistique de test de Durbin Watson a pour valeur : ",DW_stat)
+##test d'homoscédasticité des résidus par le test de White
+from statsmodels.stats.diagnostic import het_white
+white_test = het_white(results.resid,X)
+print("p-valeur du test de White : ",white_test[1])
+##On rejette l'hypothèse d'homoscédasticité des résidus (p-valeur < 5%)
+##Test de normalité des résidus (test de Jarque-Bera)
+from scipy.stats import jarque_bera
+jb_stat, p_value_JB = jarque_bera(results.resid)
+print("p-valeur du test de Jarque-Bera : ",p_value_JB)
+##On rejette l'hypothèse nulle de normalité des résidus
+
+##Donc une régression simple n'est pas interprétable dans ces conditions
+##La procédure de Cochrane-Orcutt peut être utilisée pour corriger de l'autocorrélation des erreurs
+##Elle calcule l'autocorrélation empirique des résidus (rho) puis crée les variables Yt-rhoY(t-1) et idem pour Xt et 
+##calcule l'autocrrélation des résidus de cette nouvelle régression etc jusqu'à ce qu'on obtienne une autocorrélation nulle
+##Assure plus de confiance dans les test de Student et la significativité statistique des coefficients
+from statsmodels.tsa.stattools import acf
+acf_resid = acf(results.resid)
+weights = -acf_resid[1:]
+modele_cochrane_orcutt = sm.OLS(y, X).fit(cov_type='HAC', cov_kwds={'maxlags':12, 'weights':weights})
+print(modele_cochrane_orcutt.summary())
+
+##Test de rupture? Lorsque la plage de données est grande, il est possible que le phénomène économique d'intérêt ait subi des altérations et des ruptures.
+##Le test de Chow peut être utilisé pour détecter des changements strucutrels dans les coefficients d'un modèle de régression
+from statsmodels.stats.diagnostic import linear_harvey_collier
+data["groupe"] = data.index>"2018-01-01"
+data['interaction'] = data["groupe"]*data['OBS_VALUE_x']
+#Modele avec interaction
+modele_interaction = sm.OLS(data['OBS_VALUE_y'], sm.add_constant(data[['OBS_VALUE_x','interaction']])).fit()
+modele_ss_interaction = sm.OLS(data['OBS_VALUE_y'], sm.add_constant(data[['OBS_VALUE_x']])).fit()
+chow_test = modele_ss_interaction.compare_f_test(modele_interaction)
+print(chow_test)
+##Problème d'hétéroscédasticité et d'autocorrélation dans les résidus, ne donne pas de résultats utilisables
+##Valeur de stat de 68.4, donc on s'attend tout de même à un rejet de l'hypothèse nulle
+
+
+
+##Modele à Correction d'Erreur
+##On a déjà validé l'hypothèse d'une relation de co-intégration, il faudrait vérifier que les séries considérées sont intégrées d'ordre 1
+##Test de stationnarité des séries temporelles
+data = pd.merge(energie_prix_import,IPP_tot, on='TIME_PERIOD', how="inner")
+data_diff = data.diff().dropna()
+energy_adf = adfuller(data_diff['OBS_VALUE_x'])
+IPP_adf = adfuller(data_diff['OBS_VALUE_y'])
+print('p-value',energy_adf[1])
+print('p-value',IPP_adf[1])
+##Parfait, les test de stationnarité rejettent l'hypothèse nulle de non stationanrité des séries utilisées
+##Donc les hypothèses nécessaires pour mettre en place un modèle ECM sont bien vérifiées
+##Equation de long terme
+long_terme = sm.OLS(data['OBS_VALUE_y'], sm.add_constant(data[['OBS_VALUE_x']])).fit()
+print(long_terme.summary())
+residus = long_terme.resid
+##Equation de court terme
+residus_lag = pd.Series(residus.shift(1).dropna(), name='residus')
+data_courte = pd.merge(data_diff,residus_lag, on='TIME_PERIOD', how="inner")
+court_terme = sm.OLS(data_courte['OBS_VALUE_y'], sm.add_constant(data_courte[['OBS_VALUE_x','residus']])).fit()
+print(court_terme.summary())
+##Le coefficient devant residus peut s'interpréter comme une vitesse de convergence, il est bien négatif comme attendu
+##Ici il vaut 1,4%, ce qui signifie qu'à chaque mois, la variable y comble 1,4% de l'écart à sa valeur de long terme (attention ici on a peu de variables explicatives)
+##En l'absence de chocs consécutifs, il faudrait donc 100/1.4 = 71 mois soit environ 6 ans pour un retour à l'équilibre
+
+##test de la stationnarité du résidu estimé en première étape
+res_adf = adfuller(residus)
+print('p-value',res_adf[1])
+##Problème, on a un résidu non stationnaire bien que les séries régressées soient stationnaires...
+##Attention, dans ce contexte de modèle ECM, aucune loi n'est connue pour les estimateurs, il est donc impossible d'interpréter les tests statistiques sur les coefficients
+
+
+
+##Pour limiter le biais introduit à la première étape, Stock et Watson propose des dynamic OLS, où on introduit symétriquement des variations avance et retard dans la première étape
+data = pd.merge(energie_prix_import,IPP_tot, on='TIME_PERIOD', how="inner")
+nombre_retard = 2
+for i in range(1,nombre_retard+1):
+    data[f'Delta_X_lag_{i}'] = data['OBS_VALUE_x']-data['OBS_VALUE_x'].shift(i)
+    data[f'Delta_X_lead_{i}'] = data['OBS_VALUE_x']-data['OBS_VALUE_x'].shift(-i)
+data = data.dropna()
+
+##Equation de long terme
+long_terme = sm.OLS(data['OBS_VALUE_y'], sm.add_constant(data[['OBS_VALUE_x','Delta_X_lag_1','Delta_X_lag_2','Delta_X_lead_1','Delta_X_lead_2']])).fit()
+print(long_terme.summary())
+residus = long_terme.resid
+##test de la stationnarité du résidu estimé en première étape
+res_adf = adfuller(residus)
+print('p-value',res_adf[1])
+##Toujours pas stationnaire
+
+##Equation de court terme
+##Attention il faut un peu transformer le résidu avant de l'introduire dans l'équation de court terme
+residus_trans = data['OBS_VALUE_y']-long_terme.params['OBS_VALUE_x']*data['OBS_VALUE_x']-long_terme.params['const']
+residus_lag = pd.Series(residus_trans.shift(1).dropna(), name='residus')
+data_courte = pd.merge(data_diff,residus_lag, on='TIME_PERIOD', how="inner")
+court_terme = sm.OLS(data_courte['OBS_VALUE_y'], sm.add_constant(data_courte[['OBS_VALUE_x','residus']])).fit()
+print(court_terme.summary())
+##On passe de 1,4% à 1,8% pour la force de rappel, pas de grande différence
